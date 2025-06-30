c44d284adebb1c0236832b74919cb567106bbb2a
reachonpeak <reachonpeak@gmail.com>
2025-06-30 12:04:02 +0530

    Add android-app/app/src/main/java/com/matchdil/ui/theme/Color.kt 1751265242169

---
+++ android-app/app/src/main/java/com/matchdil/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.matchdil.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red500 = Color(0xFFEF4444)
+val Red700 = Color(0xFFB91C1C)
+val Grey100 = Color(0xFFF3F4F6)
+val Grey300 = Color(0xFFD1D5DB)
+val Grey700 = Color(0xFF374151)
+val White = Color(0xFFFFFFFF)
